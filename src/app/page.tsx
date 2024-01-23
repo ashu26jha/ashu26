3bd3d0b1ae88584c2456c126f335f7f10dddd6f4
Ashutosh Jha <ashutosh26jha@gmail.com>
2024-01-23 10:25:06 +0530

    Added parallax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,96 @@
-import Navbar from '../../components/Navbar'
-import Desc from '../../components/Desc'
-import Projects from '../../components/Projects'
-import React, { useEffect } from "react"
+"use client"
+import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useRef } from "react";
 
-export default function Home() {
+export default function MultiLayerParallax() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
 
   return (
     <div>
 
-      <div className='main'>
-        <Navbar />
 
-        <Desc />
-        <Projects />
+      <div
+        ref={ref}
+        className="w-full h-screen overflow-hidden relative grid place-items-center"
+      >
+        <motion.h1
+          style={{ y: textY }}
+          className="font-bold text-white text-7xl md:text-9xl relative z-10"
+        >
+          Ashutosh.
+        </motion.h1>
+
+        <motion.div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `url(/image-full.jpg)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: backgroundY,
+          }}
+        />
+        <div
+          className="absolute inset-0 z-20"
+          style={{
+            backgroundImage: `url(/image-bottom.jpg)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+          }}
+        />
+      </div>
+
+      <div className="w-full bg-[#06141D]">
+        <div className="max-w-lg space-y-4 mx-auto py-24 text-neutral-300">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
