e4416675e78b28d306e7bc1d6aec4e45f84bf763
Ashutosh Jha <ashutosh26jha@gmail.com>
2024-01-23 14:39:46 +0530

    sections

--- components/MultiLayerParallax.tsx
+++ components/MultiLayerParallax.tsx
@@ -9,7 +9,7 @@ export default function MultiLayerParallax() {
     offset: ["start start", "end start"],
   });
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "400%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
 
   return (
     <div
@@ -20,13 +20,13 @@ export default function MultiLayerParallax() {
         style={{ y: textY }}
         className="font-bold text-white text-7xl md:text-9xl relative z-10"
       >
-        Ashutosh.
+        PARALLAX
       </motion.h1>
 
       <motion.div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: `url(/image-full.jpg)`,
+          backgroundImage: `url(/image-full.png)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
           y: backgroundY,
@@ -35,7 +35,7 @@ export default function MultiLayerParallax() {
       <div
         className="absolute inset-0 z-20"
         style={{
-          backgroundImage: `url(/image-bottom.jpg)`,
+          backgroundImage: `url(/image-bottom.png)`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
         }}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,110 @@
 "use client"
-import React from "react";
-import MultiLayerParallax from "../../components/MultiLayerParallax";
-import OpenSource from "../../components/OpenSource";
-import About from "../../components/About";
-import Projects from "../../components/Projects";
+import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useRef } from "react";
 
-export default function () {
-  
+export default function MultiLayerParallax() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+  const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
 
   return (
     <div>
-      <MultiLayerParallax />
-      <About/>
-      <OpenSource/>
-      <Projects/>
+
+
+      <div
+        ref={ref}
+        className="w-full h-screen overflow-hidden relative grid place-items-center"
+      >
+        <motion.h1
+          style={{ y: textY }}
+          className="font-bold text-white text-7xl md:text-9xl relative z-10"
+        >
+          Ashutosh.
+        </motion.h1>
+
+        <motion.div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `url(/image-full.jpg)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+            y: backgroundY,
+          }}
+        />
+        <div
+          className="absolute inset-0 z-20"
+          style={{
+            backgroundImage: `url(/image-bottom.jpg)`,
+            backgroundPosition: "bottom",
+            backgroundSize: "cover",
+          }}
+        />
+        <a href="#projects" className="z-40"><div><i className="arrow down"></i></div></a>
+      </div>
+      <div className="bg-black p-4 flex">
+        <div className="subpixel-antialiased font-medium text-3xl  text-white">
+          Ashutosh
+        </div>
+        <div className="subpixel-antialiased font-medium text-xl mt-auto ml-auto mr-6 text-white">
+          <a href="#projects">Projects</a>
+        </div>
+        <div className="subpixel-antialiased font-medium text-xl mt-auto mr-6 text-white">
+          Contributions
+        </div>
+      </div>
+
+      <div className="w-full bg-[#06141D]">
+        <div className="max-w-lg space-y-4 mx-auto py-24 text-neutral-300">
+          <div id="projects">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+              earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+              sit dolores odio excepturi facilis laboriosam officiis dolorem,
+              nobis reprehenderit molestiae.
+            </p>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quae
+            earum nobis quasi repellat. Amet facere nulla dolorum accusantium
+            sit dolores odio excepturi facilis laboriosam officiis dolorem,
+            nobis reprehenderit molestiae.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
